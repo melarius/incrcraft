16b105cc21d30bcb8deed3155b3b582494658764
Nikita <mertvuimaster@gmail.com>
2025-03-24 15:00:05 +0300

    game +

--- script_game.js
+++ script_game.js
@@ -14,27 +14,45 @@ let game = {
     blocks: 0,
     blocksGrowth: 1,
     blocksUpgLevel: 0,
+    copperore: 0,
+    copperoreGrowth: 1,
+    copperoreUpgLevel: 0,
+    tinore: 0,
+    tinoreGrowth: 1,
+    tinoreUpgLevel: 0,
+    ironore: 0,
+    ironoreGrowth: 1,
+    ironoreUpgLevel: 0,
 }
 
 const updrademulti = {
-    wood: 0,
+    wood: 1,
     plank: 11,
     stone: 31,
-    blocks: 51
+    blocks: 51,
+    copperore: 51,
+    tinore: 76,
+    ironore: 101
 }
 
 const sell = {
     wood: 1,
     plank: 10,
     stone: 30,
-    blocks: 50
+    blocks: 50,
+    copperore: 50,
+    tinore: 75,
+    ironore: 100
 }
 
 const baseprice = {
     wood: 10,
     plank: 100,
     stone: 300,
-    blocks: 500
+    blocks: 500,
+    copperore: 500,
+    tinore: 750,
+    ironore: 1000
 }
 
 const production = {
@@ -54,6 +72,10 @@ function endOfTurnCalc() {
     if (game.blocks < win_condition) {
         wood_prod_rate = (game.woodGrowth * game.woodUpgLevel) - (game.plankUpgLevel * production.plank)
         stone_prod_rate = (game.stoneGrowth * game.stoneUpgLevel) - (game.blocksUpgLevel * production.blocks)
+        game.copperore = game.copperore + game.copperoreGrowth * game.copperoreUpgLevel;
+        game.tinore = game.tinore + game.tinoreGrowth * game.tinoreUpgLevel;
+        game.ironore = game.ironore + game.ironoreGrowth * game.ironoreUpgLevel;
+
 
         if (game.wood + wood_prod_rate >= 0) {
             game.wood += (game.woodGrowth * game.woodUpgLevel) - (game.plankUpgLevel * production.plank);
@@ -74,6 +96,19 @@ function winGame() {
     clearTimeout(myTimer);
     alert("Вы достигли цели! Вы накопили " + win_condition.toString());
     myRestartTimer = setInterval(restartGameDialog, 2000);
+}
+
+function updateButtons() {
+    for (let elem of document.getElementsByClassName('base_btn')) {
+        if (elem.id.includes("Upg")) {
+            if (UpgradeCost(elem.id.substring(6).toLowerCase()) > game.money) {
+                elem.disabled = true;
+            }
+            else {
+                elem.disabled = false;
+            }
+        }
+    }
 }
 
 function sellItems(string) {
@@ -117,14 +152,6 @@ const ui = {
         rate: document.getElementById("spnWoodRate"),
         sell: document.getElementById("btnSellWood")
     },
-    plank: {
-        value: document.getElementById("spnPlankValue"),
-        upgrade_btn: document.getElementById("btnUpgPlank"),
-        upgrade: document.getElementById("spnPlankUpg"),
-        level: document.getElementById("spnPlankUpgLevel"),
-        rate: document.getElementById("spnPlankRate"),
-        sell: document.getElementById("btnSellPlank")
-    },
     stone: {
         value: document.getElementById("spnStoneValue"),
         upgrade_btn: document.getElementById("btnUpgStone"),
@@ -133,6 +160,39 @@ const ui = {
         rate: document.getElementById("spnStoneRate"),
         sell: document.getElementById("btnSellStone")
     },
+    copperore: {
+        value: document.getElementById("spnCopperOreValue"),
+        upgrade_btn: document.getElementById("btnUpgCopperOre"),
+        upgrade: document.getElementById("spnCopperOreUpg"),
+        level: document.getElementById("spnCopperOreUpgLevel"),
+        rate: document.getElementById("spnCopperOreRate"),
+        sell: document.getElementById("btnSellCopperOre")
+    },
+    tinore: {
+        value: document.getElementById("spnTinOreValue"),
+        upgrade_btn: document.getElementById("btnUpgTinOre"),
+        upgrade: document.getElementById("spnTinOreUpg"),
+        level: document.getElementById("spnTinOreUpgLevel"),
+        rate: document.getElementById("spnTinOreRate"),
+        sell: document.getElementById("btnSellTinOre")
+    },
+    ironore: {
+        value: document.getElementById("spnIronOreValue"),
+        upgrade_btn: document.getElementById("btnUpgIronOre"),
+        upgrade: document.getElementById("spnIronOreUpg"),
+        level: document.getElementById("spnIronOreUpgLevel"),
+        rate: document.getElementById("spnIronOreRate"),
+        sell: document.getElementById("btnSellIronOre")
+    },
+    plank: {
+        value: document.getElementById("spnPlankValue"),
+        upgrade_btn: document.getElementById("btnUpgPlank"),
+        upgrade: document.getElementById("spnPlankUpg"),
+        level: document.getElementById("spnPlankUpgLevel"),
+        rate: document.getElementById("spnPlankRate"),
+        sell: document.getElementById("btnSellPlank")
+    },
+    
     blocks: {
         value: document.getElementById("spnBlocksValue"),
         upgrade_btn: document.getElementById("btnUpgBlocks"),
@@ -146,6 +206,7 @@ const ui = {
 
 function updateUI() {
     ui.money.textContent = game.money;
+    updateButtons()
 
     ui.wood.value.textContent = game.wood;
     if (game.woodUpgLevel === 0) {
@@ -173,6 +234,33 @@ function updateUI() {
     }
     ui.stone.rate.textContent = game.stoneGrowth * game.stoneUpgLevel - (game.blocksUpgLevel * production.blocks);
     ui.stone.level.textContent = game.stoneUpgLevel;
+
+    ui.copperore.value.textContent = game.copperore;
+    if (game.copperoreUpgLevel === 0) {
+        ui.copperore.upgrade.textContent = baseprice.copperore;
+    } else {
+        ui.copperore.upgrade.textContent = UpgradeCost("copperore");
+    }
+    ui.copperore.rate.textContent = game.copperoreGrowth * game.copperoreUpgLevel - (game.blocksUpgLevel * production.blocks);
+    ui.copperore.level.textContent = game.copperoreUpgLevel;
+
+    ui.tinore.value.textContent = game.tinore;
+    if (game.tinoreUpgLevel === 0) {
+        ui.tinore.upgrade.textContent = baseprice.tinore;
+    } else {
+        ui.tinore.upgrade.textContent = UpgradeCost("tinore");
+    }
+    ui.tinore.rate.textContent = game.tinoreGrowth * game.tinoreUpgLevel - (game.blocksUpgLevel * production.blocks);
+    ui.tinore.level.textContent = game.tinoreUpgLevel;
+
+    ui.ironore.value.textContent = game.ironore;
+    if (game.ironoreUpgLevel === 0) {
+        ui.ironore.upgrade.textContent = baseprice.ironore;
+    } else {
+        ui.ironore.upgrade.textContent = UpgradeCost("ironore");
+    }
+    ui.ironore.rate.textContent = game.ironoreGrowth * game.ironoreUpgLevel - (game.blocksUpgLevel * production.blocks);
+    ui.ironore.level.textContent = game.ironoreUpgLevel;
 
     ui.blocks.value.textContent = game.blocks;
     if (game.blocksUpgLevel === 0) {
@@ -247,6 +335,54 @@ ui.blocks.upgrade_btn.addEventListener("click", function () {
         updateUI();
     }
 
+})
+
+ui.copperore.upgrade_btn.addEventListener("click", function () {
+    if (game.copperoreUpgLevel == 0) {
+        BuildItem("copperore");
+        updateUI();
+    } else {
+        UpgradeItem("copperore");
+        updateUI();
+    }
+
+})
+
+ui.copperore.sell.addEventListener("click", function () {
+    sellItems("copperore")
+    updateUI()
+})
+
+ui.tinore.upgrade_btn.addEventListener("click", function () {
+    if (game.tinoreUpgLevel == 0) {
+        BuildItem("tinore");
+        updateUI();
+    } else {
+        UpgradeItem("tinore");
+        updateUI();
+    }
+
+})
+
+ui.tinore.sell.addEventListener("click", function () {
+    sellItems("tinore")
+    updateUI()
+})
+
+ui.ironore.upgrade_btn.addEventListener("click", function () {
+    if (game.ironoreUpgLevel == 0) {
+        BuildItem("ironore");
+        updateUI();
+    } else {
+        UpgradeItem("ironore");
+        updateUI();
+    }
+
+})
+
+ui.ironore.sell.addEventListener("click", function () {
+    sellItems("ironore")
+    updateUI()
 })
 
 ui.blocks.sell.addEventListener("click", function () {
